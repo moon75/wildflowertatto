da8d87882e12a4f005fd1138996ee049a3a41b11
Moon <moonhance@gmail.com>
2026-06-07 02:15:11 +0700

    Use @sanity/client directly, remove next-sanity

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,4 +1,4 @@
-import { createClient } from "next-sanity";
+import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
